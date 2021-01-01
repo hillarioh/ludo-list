36ee017d36c63de1cb43215fca2436cb4b3e5128
Hillarioh <okerioh@gmail.com>
2021-01-01 17:31:27 +0300

    Update List

--- src/rows.js
+++ src/rows.js
@@ -6,9 +6,9 @@ function createData(name, shames, profile) {
 
 const rows = [
   
-  createData("Godwill Omwando", 11, {
+  createData("Godwill Omwando", 13, {
     prof: pic,
-    dates: [["15/NOV/2020", 3],["19/NOV/2020",3],["13/DEC/2020",2],["19/DEC/2020", 1],["22/DEC/2020", 1],["25/DEC/2020", 1]],
+    dates: [["15/NOV/2020", 3],["19/NOV/2020",3],["13/DEC/2020",2],["19/DEC/2020", 1],["22/DEC/2020", 1],["25/DEC/2020", 1],["31/DEC/2020", 2]],
   }),
   createData("Chris Nyabuto", 7, {
     prof: pic,
@@ -17,6 +17,14 @@ const rows = [
    createData("Anthony Masese", 6, {
     prof: pic,
     dates: [["15/NOV/2020", 1],["30/NOV/2020",1],["5/DEC/2020",2],["21/DEC/2020", 2]],
+  }),
+createData("Bravon Darwin", 6, {
+    prof: pic,
+    dates: [
+      ["1/NOV/2020", 1],
+      ["12/NOV/2020", 1],
+      ["15/NOV/2020", 1],["1/JAN/2021", 3]
+    ],
   }),
 createData("Titus Omwando", 5, {
     prof: pic,
@@ -27,21 +35,14 @@ createData("BOBO", 5, {
     dates: [["19/DEC/2020", 1],["24/DEC/2020", 4]],
   })
 ,
-  createData("Bravon Darwin", 3, {
-    prof: pic,
-    dates: [
-      ["1/NOV/2020", 1],
-      ["12/NOV/2020", 1],
-      ["15/NOV/2020", 1],
-    ],
-  }),
+  
   createData("Edwin Oroni", 3, {
     prof: pic,
     dates: [["1/DEC/2020", 3]],
   }),
-    createData("Steve Ongeri", 3, {
+    createData("Steve Ongeri", 5, {
     prof: pic,
-    dates: [["22/NOV/2020", 2],["20/DEC/2020",1]],
+    dates: [["22/NOV/2020", 2],["20/DEC/2020",1],["29/DEC/2020", 2]],
   }),
 createData("Felloh", 2, {
     prof: pic,
